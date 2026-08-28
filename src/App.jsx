c13fba5e2c2c5ad6e94f9b207bb63fa768e274ab
ashytrashbean <annalu.jansson@gmail.com>
2026-08-28 13:22:26 +0200

    Add UserInfo component import to App for user details display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import GetPostsData from './componenets/Posts/GetData'
+import UserInfo from './componenets/UserInfo/UserInfo'
 
 function App() {
     return (
+      <>
         <GetPostsData/>
+      </>
     )
 }
 
