ecc7f674c58dec4a835ce0c717a7a6104bc29416
ashytrashbean <annalu.jansson@gmail.com>
2026-08-28 12:49:46 +0200

    Refactor UserInfo component to accept userId prop and fetch user data accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import UserInfo from './componenets/UserInfo/UserInfo'
 
 function App() {
 
+  return(
     <>
-      
+      <UserInfo userId={121}/>
     </>
+  )
+    
 }
 
 export default App
--- src/componenets/UserInfo/UserInfo.jsx
+++ src/componenets/UserInfo/UserInfo.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react"
 
-export default function UserInfo({user}){
+export default function UserInfo({userId}){
 
 
-    const [users,setUser] = useState()
+    const [user,setUser] = useState(null)
 
     useEffect(()=> {
         let getData = async () => {
-            let response = await fetch(`https://dummyjson.com/users/${user.id}`);
+            let response = await fetch(`https://dummyjson.com/users/${userId}`);
             let data = await response.json();
-            setUser(data.user)
+            setUser(data)
         }
 
-        getData();
-    },[])
-    
+        if(userId){
+            getData()
+        }
+    },[userId])
+
+    if(!user){
+        return <p>Loading info....</p>
+    }
+
     return(
         <section>
             <img src={user.image} alt="" />
             <h3>{user.username}</h3>
-            <p>{user.email} | {user.adress.city}</p>
+            <p>{user.email} | {user.address?.city}</p>
         </section>)
 }
